9643aa0639a17322be3e95807a25d9088f6ff0b7
a7medalyapany <103336732+a7medalyapany@users.noreply.github.com>
2023-07-26 21:45:33 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,5 +1,43 @@
 #include "main.h"
 
+#include "main.h"
+
+/**
+ * print_integer - Prints a signed integer
+ * @num: The integer to be printed
+ * Return: Number of characters printed
+ */
+int print_integer(int num)
+{
+	int chars_print = 0;
+	int is_negative = 0;
+
+	if (num < 0)
+	{
+		is_negative = 1;
+		num = -num;
+		chars_print += write(1, "-", 1);
+	}
+
+	// Convert integer to string for printing
+	char buffer[20]; // Assuming a maximum of 20 digits for the integer
+	int idx = 0;
+	do
+	{
+		buffer[idx++] = (num % 10) + '0';
+		num /= 10;
+	} while (num > 0);
+
+	// Reverse the digits in the buffer
+	for (int i = idx - 1; i >= 0; i--)
+	{
+		chars_print += write(1, &buffer[i], 1);
+	}
+
+	return (chars_print);
+}
+
+
 /**
  * _printf - Printf function
  * @format: format.
@@ -47,6 +85,11 @@ int _printf(const char *format, ...)
 					str_len++;
 				write(1, str, str_len);
 				chars_print += str_len;
+			}
+			else if (*format == 'd' || *format == 'i')
+			{
+				int num = va_arg(list, int);
+				chars_print += print_integer(num);
 			}
 		}
 		format++;
